dea9eba00c08869540dce9cd53a3d9252bb425b4
John Smith <exampel@stanford.edu>
2013-07-09 14:23:43 -0400

    hw #3

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require( 'fs' )
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var tbuffer = fs.readFileSync( 'index.html')
+  tbuffer = fs.readFileSync( 'index.html' )
+  tstr = tbuffer.toString()  
+  response.send( tstr );
 });
 
 var port = process.env.PORT || 5000;
